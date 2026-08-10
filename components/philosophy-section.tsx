48eb3f15f8d2f9a4c49207586e89ba4bcff2741e
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-10 23:53:45 +0530

    Update philosophy-section.tsx

--- components/philosophy-section.tsx
+++ components/philosophy-section.tsx
@@ -10,7 +10,7 @@ function lerpColor(color1: string, color2: string, t: number): string {
   
     
     return {
-      r: parseInt(h.substring(0, 2), 16),
+      
       g: parseInt(h.substring(2, 4), 16),
       b: parseInt(h.substring(4, 6), 16),
     }
