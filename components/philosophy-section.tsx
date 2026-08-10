1abb2ca0a520401573ed4e5d3355d601b969a21a
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-10 23:55:45 +0530

    Update philosophy-section.tsx

--- components/philosophy-section.tsx
+++ components/philosophy-section.tsx
@@ -16,7 +16,7 @@ function lerpColor(color1: string, color2: string, t: number): string {
     }
   }
   
-  const c2 = hex2rgb(color2)
+  
   const r = Math.round(c1.r + (c2.r - c1.r) * t)
   const g = Math.round(c1.g + (c2.g - c1.g) * t)
   const b = Math.round(c1.b + (c2.b - c1.b) * t)
