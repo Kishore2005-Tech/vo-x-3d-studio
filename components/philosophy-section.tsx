5ae4e900f0ba18ac315988b903514550e3c30945
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-10 23:56:27 +0530

    Update philosophy-section.tsx

--- components/philosophy-section.tsx
+++ components/philosophy-section.tsx
@@ -8,7 +8,7 @@ import { useThemeGalleryStore, THEMES } from "./theme-gallery-section"
 
 function lerpColor(color1: string, color2: string, t: number): string {
 const hex2rgb = (hex: string) => {  
-    
+  const h = hex.replace("#", "")  
     return {
       
       
