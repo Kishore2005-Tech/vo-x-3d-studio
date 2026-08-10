c74c9fd9c689ed9be26fbe81c322d6690c84d00d
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-10 23:57:29 +0530

    Update philosophy-section.tsx

--- components/philosophy-section.tsx
+++ components/philosophy-section.tsx
@@ -12,7 +12,7 @@ const hex2rgb = (hex: string) => {
     return {
      r: parseInt(h.substring(0, 2), 16), 
      g: parseInt(h.substring(2, 4), 16), 
-     
+     b: parseInt(h.substring(4, 6), 16),
     }
   }
   
